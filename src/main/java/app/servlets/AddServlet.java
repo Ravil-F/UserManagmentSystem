d44e447d85f1f9b66c80fa6fced3453aaeea0c60
rf <rf_3088@mail.ru>
2026-02-04 20:09:19 +0300

    fix 404 code

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -1,13 +1,22 @@
 package app.servlets;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
+
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("============== ADD SERVLET INIT ==============");
+        System.out.println("Servlet Name: " + getServletName());
+        System.out.println("Servlet Config: " + getServletConfig());
+        System.out.println("==============================================");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
